46b04fa6506f4e4671089ce3a20ed739306c6331
Vinicius <solutiontecforyou@gmail.com>
2021-07-07 01:32:40 -0300

    add axios and implement it in HttpService

--- src/services/HttpService.ts
+++ src/services/HttpService.ts
@@ -1,6 +1,8 @@
 import { BaseResponse } from "../types/Baseresponse";
-import { Cliente } from "../types/Cliente";
+import { Cliente, Endereco } from "../types/Cliente";
+import axios from "axios";
 
+const api = axios.create({ baseURL: "http://127.0.0.1:8000/api" });
 class HttpService {
   private readonly URL_API = "http://127.0.0.1:8000/api";
   private catch(message = "Ocorreu algum erro") {
@@ -13,10 +15,87 @@ class HttpService {
 
   async listClients(): Promise<BaseResponse<Cliente[]>> {
     try {
-      const result: BaseResponse<Cliente[]> = await (
-        await fetch(`${this.URL_API}/clientes`)
-      ).json();
-      return result;
+      const { data } = await api.get(`/clientes`);
+      return data;
+    } catch (error) {
+      return this.catch();
+    }
+  }
+
+  async createClient(cliente: Cliente): Promise<BaseResponse<any>> {
+    try {
+      const result = await api.post(`/cliente`, cliente);
+
+      return result.data;
+    } catch (error) {
+      return this.catch();
+    }
+  }
+
+  async updateClient(cliente: Cliente, id: number): Promise<BaseResponse<any>> {
+    try {
+      const result = await api.put(`/cliente/${id}`, {
+        ...cliente,
+      });
+      return result.data;
+    } catch (error) {
+      return this.catch();
+    }
+  }
+
+  async deleteClient(id: number): Promise<BaseResponse<any>> {
+    try {
+      const result: BaseResponse<any> = await api.delete(`/cliente/${id}`);
+      return result.data;
+    } catch (error) {
+      return this.catch();
+    }
+  }
+
+  async getCliente(id: number): Promise<BaseResponse<Cliente>> {
+    try {
+      const { data } = await api.get(`/cliente/${id}`);
+      return data;
+    } catch (error) {
+      return this.catch();
+    }
+  }
+
+  async createEndereco(
+    idUser: number,
+    endereco: Endereco
+  ): Promise<BaseResponse<any>> {
+    try {
+      const { data } = await api.post(`/endereco/${idUser}`, endereco);
+      return data;
+    } catch (error) {
+      return this.catch();
+    }
+  }
+
+  async updateEndereco(
+    idUser: number,
+    idEndereco: number,
+    endereco: Endereco
+  ): Promise<BaseResponse<any>> {
+    try {
+      const { data } = await api.put(
+        `/endereco/${idUser}/${idEndereco}`,
+        endereco
+      );
+      return data;
+    } catch (error) {
+      return this.catch();
+    }
+  }
+
+  async deleteEndereco(
+    idUser: number,
+    idAddress: number
+  ): Promise<BaseResponse<any>> {
+    try {
+      const { data } = await api.delete(`/endereco/${idUser}/${idAddress}`);
+      return data;
     } catch (error) {
       return this.catch();
     }
